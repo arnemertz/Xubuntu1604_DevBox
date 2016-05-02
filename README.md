7e6fca660b68ec513f9307707addde6f49bd6ace
Arne Mertz <void@arne-mertz.de>
2016-05-02 06:56:19 +0200

    add cmake and clion to provisioning script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Xubuntu 16.04 desktop for C++ development
+
+:warning: This is work in progress, do not use yet!
+
+The base box used in the Vagrantfile will be uploaded once the installation is usable.
--- provision/provision.sh
+++ provision/provision.sh
@@ -6,3 +6,8 @@ sudo apt-get -y install clang-3.7
 PWD=$(pwd)
 cd /usr/bin && for C in $(ls clang*); do L=${C%-3.7}; T=$(readlink $C); sudo ln -s $T $L; done && cd $PWD
 
+sudo apt-get -y install cmake
+
+CLION_VERSION=2016.1.1
+sudo wget -q http://download.jetbrains.com/cpp/CLion-${CLION_VERSION}.tar.gz
+sudo tar xfz CLion-${CLION_VERSION}.tar.gz -C /opt
