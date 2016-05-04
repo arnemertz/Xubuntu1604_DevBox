aca47aec6d1f4b4c9e2982ae56cef4827b6b3891
Arne Mertz <github@arne-mertz.de>
2016-05-04 15:36:10 +0200

    add lldb, conan, fix dpkg lock issue, use public base box from atlas

--- provision/provision.sh
+++ provision/provision.sh
@@ -1,13 +1,50 @@
-apt-get update && apt-get -y upgrade && apt-get -y autoremove
+if [ -e /var/lib/dpkg/lock ]; then
+	echo -----------------------------------
+	echo REMOVE /var/lib/dpkg/lock
+	echo -----------------------------------
+	sudo rm -f /var/lib/dpkg/lock
+fi
 
+apt-get update && apt-get -y upgrade && apt-get -y autoremove	
+
+echo -----------------------------------
+echo INSTALL GIT
+echo -----------------------------------
 sudo apt-get -y install git
 
-sudo apt-get -y install clang-3.7
-PWD=$(pwd)
-cd /usr/bin && for C in $(ls clang*); do L=${C%-3.7}; T=$(readlink $C); sudo ln -s $T $L; done && cd $PWD
+echo -----------------------------------
+echo INSTALL COMPILER
+echo -----------------------------------
+CLANG_VERSION=3.7
+for PROG in clang lldb; do
+	sudo apt-get -y install ${PROG}-${CLANG_VERSION}
+	for C in $(ls /usr/bin/${PROG}*${CLANG_VERSION}); do
+		L=${C%-$CLANG_VERSION}
+		B=$(basename $L)
+		sudo update-alternatives --install $L $B $C 1000
+	done
+done
 
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
+
+echo -----------------------------------
+echo INSTALL CMAKE
+echo -----------------------------------
 sudo apt-get -y install cmake
 
+echo -----------------------------------
+echo DOWNLOAD CLION
+echo -----------------------------------
 CLION_VERSION=2016.1.1
 sudo wget -q http://download.jetbrains.com/cpp/CLion-${CLION_VERSION}.tar.gz
 sudo tar xfz CLion-${CLION_VERSION}.tar.gz -C /opt
+
+echo -----------------------------------
+echo INSTALL CONAN
+echo -----------------------------------
+sudo apt-get -y install python-pip
+sudo pip install conan
+su -l -c conan vagrant
+sed -i "s/compiler=gcc/compiler=clang/; s/version=[0-9.]*/version=$CLANG_VERSION/" ~vagrant/.conan/conan.conf
+
